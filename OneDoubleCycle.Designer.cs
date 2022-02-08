f279c91c739d432472c91d9e10e20180299dac0f
Alexandr Roslyakov <99288384+KingDog25@users.noreply.github.com>
2022-02-09 02:40:52 +0300

    Add files via upload

---
+++ OneDoubleCycle.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace _23_variant_CSharp_2_5_2
+{
+    partial class OneDoubleCycle
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMatrDim = new System.Windows.Forms.Button();
+            this.labelMinSum = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelInputEl = new System.Windows.Forms.Label();
+            this.textBoxInputEl = new System.Windows.Forms.TextBox();
+            this.radioButtonRandom = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonMatrDim
+            // 
+            this.buttonMatrDim.Location = new System.Drawing.Point(397, 15);
+            this.buttonMatrDim.Name = "buttonMatrDim";
+            this.buttonMatrDim.Size = new System.Drawing.Size(103, 26);
+            this.buttonMatrDim.TabIndex = 23;
+            this.buttonMatrDim.Text = "Применить";
+            this.buttonMatrDim.UseVisualStyleBackColor = true;
+            this.buttonMatrDim.Click += new System.EventHandler(this.buttonMatrDim_Click);
+            // 
+            // labelMinSum
+            // 
+            this.labelMinSum.AutoSize = true;
+            this.labelMinSum.Font = new System.Drawing.Font("Segoe UI", 9.163636F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelMinSum.Location = new System.Drawing.Point(13, 418);
+            this.labelMinSum.Name = "labelMinSum";
+            this.labelMinSum.Size = new System.Drawing.Size(0, 19);
+            this.labelMinSum.TabIndex = 22;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 82);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 47;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1030, 519);
+            this.dataGridView1.TabIndex = 21;
+            // 
+            // labelInputEl
+            // 
+            this.labelInputEl.AutoSize = true;
+            this.labelInputEl.Font = new System.Drawing.Font("Segoe UI", 9.163636F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labelInputEl.Location = new System.Drawing.Point(-1, 18);
+            this.labelInputEl.Name = "labelInputEl";
+            this.labelInputEl.Size = new System.Drawing.Size(257, 19);
+            this.labelInputEl.TabIndex = 20;
+            this.labelInputEl.Text = "Введите размерность матрицы N:";
+            // 
+            // textBoxInputEl
+            // 
+            this.textBoxInputEl.Location = new System.Drawing.Point(301, 15);
+            this.textBoxInputEl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxInputEl.Name = "textBoxInputEl";
+            this.textBoxInputEl.Size = new System.Drawing.Size(75, 26);
+            this.textBoxInputEl.TabIndex = 19;
+            // 
+            // radioButtonRandom
+            // 
+            this.radioButtonRandom.AutoSize = true;
+            this.radioButtonRandom.Checked = true;
+            this.radioButtonRandom.Location = new System.Drawing.Point(12, 52);
+            this.radioButtonRandom.Name = "radioButtonRandom";
+            this.radioButtonRandom.Size = new System.Drawing.Size(315, 23);
+            this.radioButtonRandom.TabIndex = 24;
+            this.radioButtonRandom.TabStop = true;
+            this.radioButtonRandom.Text = "Заполнить матрицу случайными элементами";
+            this.radioButtonRandom.UseVisualStyleBackColor = true;
+            // 
+            // OneDoubleCycle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonRandom);
+            this.Controls.Add(this.buttonMatrDim);
+            this.Controls.Add(this.labelMinSum);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelInputEl);
+            this.Controls.Add(this.textBoxInputEl);
+            this.Name = "OneDoubleCycle";
+            this.Text = "Алгоритм с использованием единственного двухкратного цикла";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMatrDim;
+        private System.Windows.Forms.Label labelMinSum;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelInputEl;
+        private System.Windows.Forms.TextBox textBoxInputEl;
+        private System.Windows.Forms.RadioButton radioButtonRandom;
+    }
+}
+
---
+++ OneDoubleCycle.cs
@@ -0,0 +1,60 @@
+﻿/*Условие:Дана матрица, состоящая из n строк и n столбцов или одномерные массивы из n элементов каждый Составить алгоритм и программу для получения 
+и выводов указанных в условии результатов и самих массивов, если изменялись какие-либо их элементы. Алгоритм любой задачи может быть составлен
+с использованием единственного двухкратного цикла*/
+/*Задача 2.5.2 Условие: Получить массив X1, X2 ...Xn, По правилу Xi = 1, если элемент Aii больше каждого из элементов i-ой строки матрицы,
+иначе Xi = 0. Найти так же сумму элементов матрицы.*/
+
+/*Condition: Given a matrix consisting of n rows and n columns or one-dimensional arrays of n elements each Compose an algorithm and a program to obtain
+and the outputs of the results specified in the condition and the arrays themselves, if any of their elements have changed. The algorithm of any problem can be composed
+using a single double loop*/
+/*Task 2.5.2 Condition: Get an array X1, X2 ...Xn, According to the rule Xi = 1, if the element Aii is greater than each of the elements of the i-th row of the matrix,
+otherwise Xi = 0. Also find the sum of the matrix elements.*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23_variant_CSharp_2_5_2
+{
+    public partial class OneDoubleCycle : Form
+    {
+        public OneDoubleCycle()
+        {
+            InitializeComponent();
+        }
+        private void buttonMatrDim_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBoxInputEl.Text);                 //n x n матрица
+            dataGridView1.RowCount = n+1;                             //кол-во строк(+1 получ. массив)
+            dataGridView1.ColumnCount = n;                          //столбцов
+            dataGridView1.Rows[0].HeaderCell.Value = "Матрица N x N";
+            int SummElemets = 0;									//для подсчета суммы элементов матрицы
+            Random rnd = new Random();
+            int[,] tableNxN = new int[n, n];
+            bool[] tableNewN = new bool[n];                           //одномерный массив для вывода результатов
+            bool Condition = true;									//флаг для хранения условия
+
+            for (int count_row = 0; count_row < n; count_row++)
+            {
+                for (int count_column = 0; count_column < n; count_column++)                                         //заполняем значения X
+                {
+                    tableNxN[count_row, count_column] = rnd.Next(-25, 25);                                      //заполняем массив случайными значениями координат
+                    dataGridView1.Rows[count_row].Cells[count_column].Value = tableNxN[count_row, count_column];
+                    SummElemets += tableNxN[count_row, count_column];
+                    if (tableNxN [count_row, count_row] < tableNxN[count_row, count_column])                     //Если элемент Aii меньше хоть одного текущего элемента i-ой строки матрицы
+                        Condition = false;
+                }
+                tableNewN[count_row] = Condition;
+                dataGridView1.Rows[n].Cells[count_row].Value = tableNewN[count_row];
+                dataGridView1.Rows[count_row].Cells[count_row].Style.BackColor = Color.LightYellow;
+                Condition = true;                                      //сбросить флаг
+            }
+        }
+    }
+}
